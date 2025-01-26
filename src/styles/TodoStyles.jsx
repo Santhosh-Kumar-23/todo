8fb5a6e823cdd1d6c116b610ff13768f193928b9
Santhosh-Kumar-23 <99msanthosh@gmail.com>
2025-01-26 23:06:52 +0530

    ui issue fixed in todo list page

--- src/styles/TodoStyles.jsx
+++ src/styles/TodoStyles.jsx
@@ -1,9 +1,10 @@
 import { StyleSheet } from "react-native";
+import colors from "../utlis/Colors";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor:colors.lightGray,
     paddingTop: 40,
     paddingHorizontal: 16,
   },
